e5a703bf129577529e591bf457f4c7aca024dd46
Zebulun Arendsee <arendsee@iastate.edu>
2015-10-12 01:25:20 -0500

    Add rough outline of OO dataset structure

---
+++ R/dataClass.R
@@ -0,0 +1,44 @@
+## Container class
+# Fields
+#  datalist
+#  filter
+#  order
+# Methods
+#  getDT
+#  getNamesOf(types)
+#  getDataof(types)
+#  addFilter(logical.vector)
+#  clearFilter
+
+## Data parent class
+# Fields
+#  value
+#  container
+# Methods
+#  value(filter=TRUE)
+#  asCat
+#  asNum
+
+## Num class
+# Fields
+# Methods
+#   asCat
+
+## Cat class
+# Fields
+# Methods
+
+## Seq class
+# Fields
+# Methods
+
+## Cor class
+# Fields
+#  mat
+# Methods
+
+## LongCat class
+# Fields
+# Methods
+#  asCat
+#  asNum
