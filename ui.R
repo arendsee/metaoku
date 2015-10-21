a5e178cfeac0fcef2ea55a91aec95792d91a7702
Zebulun Arendsee <arendsee@iastate.edu>
2015-10-20 23:41:20 -0500

    Refactor ui.R

--- ui.R
+++ ui.R
@@ -5,63 +5,95 @@ require(markdown)
 
 source('config')
 
+home_panel <- tabPanel(
+    'Home',
+    shiny::includeMarkdown(config$home_tab)
+)
+
+select_panel <- tabPanel(
+    'Select',
+    sidebarLayout(
+        sidebarPanel(
+            radioButtons('selected.dataset', 'Select a dataset', c('None' = 'none')),
+            uiOutput('dataset_description'),
+            downloadButton('downloadProject', 'Download')
+        ),
+        mainPanel(DT::dataTableOutput("column_table"))
+    )
+)
+
+view_panel <- tabPanel(
+    'View',
+    sidebarLayout(
+        sidebarPanel(
+            plotOutput('view_data_plot'),
+            fluidRow(
+                column(2, checkboxInput('logx', 'log2 x-axis')),
+                column(2, checkboxInput('logy', 'log2 y-axis')),
+                column(4, selectInput('compare.to', 'Compare to', choices='None')),
+                column(4, selectInput('group.by', 'Group by', choices='None'))),
+            fluidRow(
+                column(8, textInput(inputId='user_ids',
+                                    label='Enter ids',
+                                    value=NULL)),
+                column(4, selectInput('user_key', 'Select Key', choices='None'))),
+            downloadButton('downloadData', 'Download')
+        ),
+        mainPanel(DT::dataTableOutput("main_table"))
+    )
+)
+
+plot_panel <- tabPanel(
+    'Plot',
+    sidebarLayout(
+        sidebarPanel(
+            uiOutput('plot.sidebar'),
+            actionButton('build.plot', 'Plot')
+        ),
+        mainPanel(
+            plotOutput('plot_data_plot', height='800px')
+        )
+    )
+)
+
+upload_panel <- tabPanel(
+    'Upload',
+    sidebarLayout(
+        sidebarPanel(
+            fileInput('upload.file', 'Filename', multiple=TRUE),
+            radioButtons('upload.type', 'Select upload type',
+                         choices=list('Single Table'='single', 'Dataset'='dataset')),
+            textOutput('upload.status')
+        ),
+        mainPanel(
+            uiOutput('upload.instructions')
+        )
+    )
+)
+
+help_panel <- tabPanel(
+    'Help',
+    shiny::includeMarkdown(config$help_tab)
+)
+
+about_panel <- tabPanel(
+    'About',
+    shiny::includeMarkdown('VERSION'),
+    shiny::includeMarkdown(config$about_tab)
+)
+
 # Define UI for dataset viewer application
 shinyUI(
     fluidPage(
         theme = shinytheme('spacelab'),
         tabsetPanel(
-            tabPanel('Home', shiny::includeMarkdown(config$home_tab)),
-            tabPanel('Select', sidebarLayout(
-                sidebarPanel(
-                    radioButtons('selected.dataset', 'Select a dataset', c('None' = 'none')),
-                    uiOutput('dataset_description'),
-                    downloadButton('downloadProject', 'Download')
-                ),
-                mainPanel(
-                    DT::dataTableOutput("column_table")
-                )
-            )),
-            tabPanel('View', sidebarLayout(
-                sidebarPanel(
-                    plotOutput('view_data_plot'),
-                    fluidRow(
-                        column(2, checkboxInput('logx', 'log2 x-axis')),
-                        column(2, checkboxInput('logy', 'log2 y-axis')),
-                        column(4, selectInput('compare.to', 'Compare to', choices='None')),
-                        column(4, selectInput('group.by', 'Group by', choices='None'))),
-                    fluidRow(
-                        column(8, textInput(inputId='user_ids',
-                                            label='Enter ids',
-                                            value=NULL)),
-                        column(4, selectInput('user_key', 'Select Key', choices='None'))),
-                    downloadButton('downloadData', 'Download')
-                ),
-                mainPanel(DT::dataTableOutput("main_table"))
-            )),
-            tabPanel('Plot', sidebarLayout(
-                sidebarPanel(
-                    uiOutput('plot.sidebar'),
-                    actionButton('build.plot', 'Plot')
-                ),
-                mainPanel(
-                    plotOutput('plot_data_plot', height='800px')
-                )
-            )),
-            tabPanel('Upload', sidebarLayout(
-                sidebarPanel(
-                    fileInput('upload.file', 'Filename', multiple=TRUE),
-                    radioButtons('upload.type', 'Select upload type',
-                                 choices=list('Single Table'='single', 'Dataset'='dataset')),
-                    textOutput('upload.status')
-                ),
-                mainPanel(
-                    uiOutput('upload.instructions')
-                )
-            )),
-            tabPanel('Help', shiny::includeMarkdown(config$help_tab)),
-            tabPanel('About',
-                shiny::includeMarkdown('VERSION'),
-                shiny::includeMarkdown(config$about_tab))
+            home_panel,
+            select_panel,
+            view_panel,
+            plot_panel,
+            upload_panel,
+            help_panel,
+            about_panel
         )
     )
 )
